cb001aaf2b8ef6ebf298e8a46ae334a32b498af7
Dmitriy Balobanov <balobanov.dmitriy.78@gmail.com>
2024-11-13 17:53:54 +0300

    fix(composer): явно указал версию 1 и выше для phpstan/phpdoc-parser
    Добавил тест в воркфлоу для проверки проекта без дев зависимостей

---
+++ composer-unused.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use ComposerUnused\ComposerUnused\Configuration\Configuration;
+use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
+
+return static function (Configuration $config): Configuration {
+    $config
+        ->addNamedFilter(NamedFilter::fromString('phpdocumentor/reflection-docblock'))
+        ->addNamedFilter(NamedFilter::fromString('phpstan/phpdoc-parser'))
+        ->addNamedFilter(NamedFilter::fromString('symfony/property-access'))
+    ;
+
+    return $config;
+};
