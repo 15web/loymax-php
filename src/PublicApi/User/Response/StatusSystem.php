191a212f682e0dbb4b3c2f65bd8de7b63c43012c
Pavel Prokofiev <ps.prokofiev@gmail.com>
2025-01-27 15:05:39 +0300

    fix(status-system): исправил получение статусных систем

--- src/PublicApi/User/Response/StatusSystem.php
+++ src/PublicApi/User/Response/StatusSystem.php
@@ -12,17 +12,19 @@ final readonly class StatusSystem
 {
     /**
      * @param non-empty-string $name Название статусной системы
+     * @param non-empty-string $logicalName Логическое имя статусной системы
      * @param string $description Описание статусной системы
      * @param list<StatusItem> $statuses Список всех статусов статусной системы
-     * @param StatusItem $currentStatus Текущий статус клиента
+     * @param StatusItem|null $currentStatus Текущий статус клиента
      * @param float $currentValue Текущее значение счетчика
      * @param float $nextStatusValue Значение счетчика для перехода на следующий статус
      */
     public function __construct(
         public string $name,
+        public string $logicalName,
         public string $description,
         public array $statuses,
-        public StatusItem $currentStatus,
+        public ?StatusItem $currentStatus,
         public float $currentValue,
         public float $nextStatusValue,
     ) {}
